4a9c1551d3cbd52ec9ff66a0d946f0d1e481aee0
PEDRO AKSARLIAN <49554455@ort>
2026-04-21 08:13:48 -0300

    Finalización de la clase Grupo y del Model

--- Models/Grupo.cs
+++ Models/Grupo.cs
@@ -30,5 +30,30 @@ public class Grupo
         interesesUriel.Add("Jugar al padel");
         integrantes.Add(50156463, new Integrante("Uriel", "imagenUriel.jpg", 50156463, new DateTime(2010,04,04), familiaresUriel, interesesUriel));
 
+        List<string>familiaresTobias = new List<string>();
+        familiaresTobias.Add("Mamá: Constanza");
+        familiaresTobias.Add("Papá: Javier");
+        familiaresTobias.Add("Hermano: Manuel");
+        List<string>interesesTobias = new List<string>();
+        interesesTobias.Add("Comer hamburguesas");
+        interesesTobias.Add("Mirar peliculas");
+        interesesTobias.Add("Hacer boxeo");
+        integrantes.Add(49905586, new Integrante("Tobias", "imagenTobias.jpg", 49905586, new DateTime(2009,11,10), familiaresTobias, interesesTobias));
     }
+
+    Dic<int, integrantes> devolverIntegrantes()
+    {
+        return integrantes;
+    }
+    
+    Integrante getIntegrante(int dni)
+    {
+        bool resultado = integrantes.ContainsKey(dni);
+        if(resultado)
+        {
+            return integrantes[dni];
+        }else{
+            return null;
+        }
+        
 }
